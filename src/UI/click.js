61514d3199b7cec8d73b9b7745e8ecc54178f218
Belal Mahmoud <b.belal.esmail.95@gmail.com>
2026-04-10 01:15:18 +0200

    Applying moveing and highlighting legal moves

--- src/UI/click.js
+++ src/UI/click.js
@@ -1,19 +1,38 @@
 import { boardState } from "../Logic/boardState.js";
+import { getLegalMoves } from "../Logic/moves/index.js";
 import { square } from "./square.js";
+import { render } from "./render.js";
 
 //Click section
 let selectedPiece = null;
 let turn = "white";
+let moveList = [];
 const deselect = (piece) => {
   if (piece) square.removeClass(piece?.index, "selected");
+  moveList.forEach((i) => {
+    square.removeClass(i, "validMove");
+  });
   selectedPiece = null;
 };
 const select = (piece) => {
-  square.addClass(piece?.index, "selected");
+  if (piece) square.addClass(piece?.index, "selected");
+  const moves = getLegalMoves(piece, boardState);
+  moves.forEach((i) => {
+    square.addClass(i, "validMove");
+  });
   selectedPiece = piece;
+  moveList = moves;
 };
-const move = () => {};
-const validMove = () => {};
+const move = (index) => {
+  boardState[index] = selectedPiece;
+  const i = selectedPiece.index;
+  deselect(selectedPiece);
+  boardState[index].index = index;
+  boardState[i] = null;
+  render();
+  turn = turn === "white" ? "black" : "white";
+};
+const validMove = (index) => moveList.includes(index);
 export function handleClick(e) {
   /**
    * @type {HTMLElement} The current square clicked by the user
@@ -25,23 +44,22 @@ export function handleClick(e) {
     // If a piece is not selected
     if (pieceColor === turn) {
       // Clicked Own Piece
-      console.log("clicked square is own");
       select(clickedPiece);
     } else {
       // Clicked enemy or empty piece
     }
   } else {
     // If a piece was selected
-    if (selectedPiece == clickedPiece) {
+    if (selectedPiece === clickedPiece) {
       // Clicked Same Piece
       deselect(selectedPiece);
     } else if (pieceColor === turn) {
       // Clicked different own piece
       deselect(selectedPiece);
       select(clickedPiece);
-    } else if (validMove()) {
+    } else if (validMove(index)) {
+      move(index);
       // Clicked on a valid square to move
-      move();
     } else {
       // Clicked on an invalid square
       deselect(selectedPiece);
