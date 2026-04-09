99d04d976bb99c7aa85ad9ae3ac5a73ca1f29eab
Belal Mahmoud <b.belal.esmail.95@gmail.com>
2026-04-09 14:59:33 +0200

    Remove comments

--- src/UI/render.js
+++ src/UI/render.js
@@ -1,9 +1,10 @@
 import { pieces } from "../Data/symbols.js";
 import { boardState } from "../Logic/boardState.js";
-
+/**
+ * Render Pieces from the board state to the ui board
+ */
 export function render() {
   for (let i = 0; i < boardState.length; i++) {
-    // const { row, col } = Board.getSquarePosition(i);
     const square = document.querySelector(`[index="${i}"]`);
     if (boardState[i] === null) square.innerHTML = null;
     else {
