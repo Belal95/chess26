b46110147956f9a1938ffed13942b72e9c50c84c
Belal Mahmoud <b.belal.esmail.95@gmail.com>
2026-04-13 20:34:28 +0200

    Adding a separate color for captures

--- src/UI/UI.js
+++ src/UI/UI.js
@@ -1,14 +1,17 @@
+import { boardState } from "../Logic/boardState.js";
 import { square } from "./square.js";
-
+const state = boardState.get();
 const select = (piece, moves) => {
   if (piece) square.addClass(piece?.index, "selected");
   moves.forEach((i) => {
-    square.addClass(i, "validMove");
+    if (!!state[i]) square.addClass(i, "capture");
+    else square.addClass(i, "validMove");
   });
 };
 const deselect = (piece, moves) => {
   if (piece) square.removeClass(piece?.index, "selected");
   moves.forEach((i) => {
+    if (!!state[i]) square.removeClass(i, "capture");
     square.removeClass(i, "validMove");
   });
 };
