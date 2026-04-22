e9714d46f60be6e94aad00d81ab505c3c0349547
Belal Mahmoud <b.belal.esmail.95@gmail.com>
2026-04-22 15:19:06 +0200

    Update checkedKing.js

--- src/Logic/checkedKing.js
+++ src/Logic/checkedKing.js
@@ -1,5 +1,10 @@
 import { getRawMoves } from "./moves/index.js";
-
+/**
+ *
+ * @param {'white'|'black'} color
+ * @param {[{color,type,index,hasMoved}]} boardState
+ * @returns
+ */
 export const checkedKing = (color, boardState) => {
   const king = boardState.find(
     (piece) => piece?.type === "king" && piece?.color === color,
