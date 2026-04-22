ae7d3c606eea267d5b0a0664ac6d8a432ecb09e8
Belal Mahmoud <b.belal.esmail.95@gmail.com>
2026-04-22 15:20:18 +0200

    Update to remove castling condition

--- src/Logic/movePiece.js
+++ src/Logic/movePiece.js
@@ -8,5 +8,6 @@
 export const movePiece = (boardState, from, to) => {
   boardState[to] = boardState[from];
   boardState[to].index = to;
+  if ("hasMoved" in boardState[to]) boardState[to].hasMoved = true;
   boardState[from] = null;
 };
