df1e35bb16f68978ab8b7b4b527b5e5af4c1f7b1
Belal Mahmoud <b.belal.esmail.95@gmail.com>
2026-05-03 15:54:37 +0300

    Initialize get en passant moves

---
+++ src/Logic/moves/enPassant.js
@@ -0,0 +1,10 @@
+/**
+ *   @import {Piece} from "../../types.js"
+ */
+
+/**
+ *
+ * @param {Piece} piece object holds the piece type,color and index in the state
+ * @param {(Piece|Null)[]} boardState Holds the state of the board
+ */
+function getEnPassantMoves(piece, boardState) {}
