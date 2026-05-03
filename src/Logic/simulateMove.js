865639cb29a252941a1c83e6d9668fe27ad322dc
Belal Mahmoud <34943909+Belal95@users.noreply.github.com>
2026-05-03 23:52:25 +0300

    remove history from simulate move

--- src/Logic/simulateMove.js
+++ src/Logic/simulateMove.js
@@ -12,6 +12,6 @@ import { movePiece } from "./movePiece.js";
  */
 export const simulateMove = (boardState, from, to) => {
   const copyState = structuredClone(boardState);
-  movePiece(copyState, from, to);
+  movePiece(copyState, from, to, false);
   return copyState;
 };
