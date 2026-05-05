bf4a65f7106cd5e1836a7d575061a076520154ef
Belal Mahmoud <b.belal.esmail.95@gmail.com>
2026-05-05 15:45:43 +0300

    Create a reset function for board state

--- src/Logic/boardState.js
+++ src/Logic/boardState.js
@@ -7,7 +7,6 @@ import { getRowFirstIndex } from "./helpers.js";
  * @type {(Piece|Null)[]} The initial board state
  */
 const state = new Array(64).fill(null);
-
 /**
  * Fills board state black or white back row with pieces, takes row number and pieces color
  * @param {Number} row - row number that needs to be filled
@@ -46,6 +45,18 @@ const fillPawnRow = (row, color) => {
     index++;
   }
 };
+/**
+ *
+ */
+function resetBoardState() {
+  state.length = 0;
+  state.length = 64;
+  state.fill(null, 0, 64);
+  fillBackRow(1, "black");
+  fillPawnRow(2, "black");
+  fillPawnRow(7, "white");
+  fillBackRow(8, "white");
+}
 
 fillBackRow(1, "black");
 fillPawnRow(2, "black");
@@ -55,4 +66,4 @@ fillBackRow(8, "white");
 const get = () => state;
 const getCopy = () => structuredClone(state);
 
-export const boardState = { get, getCopy };
+export const boardState = { get, getCopy, resetBoardState };
